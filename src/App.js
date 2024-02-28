ee648218a55e86c4177b0d4c2c1ef34473f36f5c
Charitha Wickramarathna <56013746+Charitha615@users.noreply.github.com>
2024-02-28 13:35:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Charitha
+          Learn React Charitha suranga
         </a>
       </header>
     </div>
